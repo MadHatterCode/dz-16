81e45b3989bc3874eb9dcc626a3197ddfd1b22b5
Sergii <sadante@gmail.com>
2024-03-09 12:07:20 +0200

    updated solution

--- dz-16.py
+++ dz-16.py
@@ -1,5 +1,8 @@
 def say_hi(name, age):
-    return print(f"Hi. My name is {name} and I'm {age} years old")
+    if isinstance(name, str) and isinstance(age, int):
+        return print(f"Hi. My name is {name} and I'm {age} years old")
+    else:
+        return print("Please, provide correct arguments")
 
 
 say_hi("Sergii", 30)
